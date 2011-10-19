829061b399e155f918bc2f5168872cee8c2f7782
Fabricio C Zuardi <fabricio@fabricio.org>
2011-10-19 16:11:51 -0200

    ie8 para cima sao os que nao recebem o aviso do chrome frame

--- wordpress/wp-content/themes/infocards/footer.php
+++ wordpress/wp-content/themes/infocards/footer.php
@@ -48,7 +48,7 @@
 	</footer><!-- #colophon -->
 </div><!-- #page -->
 <script src="<?php echo get_template_directory_uri(); ?>/js/prefixfree.min.js" type="text/javascript"></script>
-<!--[if lt IE 7 ]>
+<!--[if lt IE 8 ]>
 <script defer src="//ajax.googleapis.com/ajax/libs/chrome-frame/1.0.3/CFInstall.min.js"></script>
 <script defer>window.attachEvent('onload',function(){CFInstall.check({mode:'overlay'})})</script>
 <![endif]-->
